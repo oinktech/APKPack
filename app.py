3d30a6a0a018c3e652766dd374f8add7cb363633
oinktech <oinktech2024@gmail.com>
2024-10-11 22:38:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -151,4 +151,4 @@ def upload_file():
         shutil.rmtree(BUILD_FOLDER, ignore_errors=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
